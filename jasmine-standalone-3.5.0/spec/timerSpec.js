2fa788af4da79ee0f77a7355717220a2d8295311
Asia <asiaellis5@gmail.com>
2020-05-12 17:28:27 +0100

    time test passed

--- jasmine-standalone-3.5.0/spec/timerSpec.js
+++ jasmine-standalone-3.5.0/spec/timerSpec.js
@@ -1,3 +1,14 @@
 describe("Timer", function () {
+  let timer;
 
+  beforeEach(function () {
+    timer = new Timer(sort)
+  })
+
+  describe("time", function () {
+    it("times the amount of time taken and pushes it into an array", function () {
+      timer.time(1, 10)
+      expect(timer.times.length).toEqual(10)
+    })
+  })
 })
